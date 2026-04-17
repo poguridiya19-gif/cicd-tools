68d4e506a5e97d0ae03adc76648a3127d50600c8
Poguri Diya <diya>
2026-04-17 16:20:59 +0530

    jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-
-# for logging
 exec > /var/log/user-data.log 2>&1
 set -x
 
+sleep 30
 # Try disk resize but don't stop script
-growpart /dev/nvme0n1 4 || true
-lvextend -L +10G /dev/mapper/RootVG-varVol || true
-lvextend -L +10G /dev/mapper/RootVG-rootVol || true
-lvextend -l +100%FREE /dev/mapper/RootVG-homeVol || true
+growpart /dev/nvme0n1 4 
+lvextend -L +10G /dev/mapper/RootVG-varVol 
+lvextend -L +10G /dev/mapper/RootVG-rootVol 
+lvextend -l +100%FREE /dev/mapper/RootVG-homeVol 
 
-xfs_growfs / || true
-xfs_growfs /var || true
-xfs_growfs /home || true
+xfs_growfs / 
+xfs_growfs /var 
+xfs_growfs /home 
 
 # jenkins install
 curl -L -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+# ADD THIS 
+yum clean all
+yum makecache
 
 # install java
 yum install fontconfig java-21-openjdk -y
